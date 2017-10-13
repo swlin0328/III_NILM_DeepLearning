78df8a8b193b0e0557ed461825e652fa8b99f2aa
changfangyi <changfangyi12965@gmail.com>
2017-10-13 16:40:57 +0800

    Add files via upload

--- prediction_json.py
+++ prediction_json.py
@@ -11,7 +11,7 @@ import requests
 import numpy as np
 import pandas as pd
 
-from keras.models import load_model
+#from keras.models import load_model
 
 # for json
 import io
@@ -22,26 +22,29 @@ try:
 except NameError:
     to_unicode = str
 
+from keras import backend as K
+
 
 # Parameters
-URL = None
+
 RAW_DATA_LIST = None
 APPLIANCE_TYPE = None
 MODEL_FILENAME = None
 
 SEQ_LENGTH = 60
-DATA_LENGTH = 60
-NUM_SEQ_PER_BATCH = 24
 
 ### main ###
 def main():
+    set_keras_backend("theano")
+    set_log_level()
     parse_args() 
     Prediction_Evaluate()
+    set_keras_backend("tensorflow")
     print('Done !')
 
 ### arse_args ###
-def arse_args():
-    global URL, RAW_DATA_LIST, APPLIANCE_TYPE, MODEL_FILENAME
+def parse_args():
+    global  RAW_DATA_LIST, APPLIANCE_TYPE, MODEL_FILENAME
     parser = argparse.ArgumentParser()
     required_named_arguments = parser.add_argument_group('required named arguments')
     required_named_arguments.add_argument('-u', '--url',
@@ -54,32 +57,33 @@ def arse_args():
 
     args = parser.parse_args()
     url = args.url
-    URL = json.loaded(urllib2.urlopen(url))
+    URL = json.load(urllib2.urlopen(url))
     RAW_DATA_LIST = URL['raw_data_list']
-    appliance_type = URL['appliance_type']
+    appliance_type = str(URL['appliance_type'])
     APPLIANCE_TYPE = RENAME(appliance_type)
+    MODEL = args.model
 
-    if  MODEL==None:
+    if  MODEL== None:
         MODEL_FILENAME= os.path.join( dirs.MODELS_DIR, APPLIANCE_TYPE + '.h5')
     else :
         MODEL_FILENAME= os.path.join( dirs.MODELS_DIR, MODEL + '.h5')
         
 
 ### RENAME ###
-def RENAME(appliance_type):
-    if target_appliance == 0002 :
+def RENAME(target_appliance):
+    if target_appliance == '0002' :
         target_appliance = 'television' 
 
-    if target_appliance == 0003 :
+    if target_appliance == '0003' :
         target_appliance = 'fridge' 
     
-    if target_appliance == 0004 :
-        target_appliance = 'air conditioner'
+    if target_appliance == '0004' :
+        target_appliance = 'air_conditioner'
 
-    if target_appliance == 0005 :
+    if target_appliance == '0005' :
         target_appliance = 'bottle warmer' 
 
-    if target_appliance == 0006 :
+    if target_appliance == '0006' :
         target_appliance = 'washing machine' 
 
     return target_appliance
@@ -87,14 +91,18 @@ def RENAME(appliance_type):
 
 ### Prediction_Evaluate ###
 def Prediction_Evaluate():
+
     # load the prediction raw data
     print('Loading data...')
-    df_aggregate = pd.DataFrame(np.zeros(DATA_LENGTH),columns=['w'])
-    df_aggregate['w'] = RAW_DATA_LIST[0:DATA_LENGTH]
+    NUM_SEQ_PER_BATCH = len(RAW_DATA_LIST)//SEQ_LENGTH
+    L = NUM_SEQ_PER_BATCH*SEQ_LENGTH
+    df_aggregate = pd.DataFrame(np.zeros(L),columns=['w'])
+    df_aggregate['w'][0:L] = RAW_DATA_LIST[0:L]
     df_aggregate = df_aggregate.fillna(0)
     df_aggregate=df_aggregate['w'].reshape(NUM_SEQ_PER_BATCH,SEQ_LENGTH,1)
 
     # Keras..
+    from keras.models import load_model
     model=load_model(MODEL_FILENAME)
     prediction = model.predict_on_batch(df_aggregate)
 
@@ -102,16 +110,34 @@ def Prediction_Evaluate():
     print('Creating test result ... \n', end='')
     output_dir = os.path.join(dirs.OUTPUT_DIR,'test_result_' + strftime('%Y-%m-%d_%H-%M-%s'))
     os.makedirs(output_dir)
-    prediction = prediction.reshape(1440,1)
+    prediction = prediction.reshape(L,1)
     prediction = prediction.tolist()
+ 
+
     APPLIANCE_USAGE = { "Appliance_usage": prediction }
     print('Saving in Json file...')
     with io.open( os.path.join(output_dir,'Appliance_usage.json') ,'w' ,encoding='utf8') as outfile:
         str_ = json.dumps(APPLIANCE_USAGE)
         outfile.write(to_unicode(str_))
     # delete the existing model
+    print ('Deleting the existing model...')
     del model 
 
+def set_log_level():
+    # hide warning log
+    import os
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    # ignore UserWarning log
+    import warnings
+    warnings.filterwarnings("ignore")
+
+def set_keras_backend(backend):
+    
+    if K.backend() != backend:
+        os.environ['KERAS_BACKEND'] = backend
+        reload(K)
+        assert K.backend() == backend
 
 if __name__ == '__main__':
     main()
